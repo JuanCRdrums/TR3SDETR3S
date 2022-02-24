40dc503c086cd1c1eebc54afcfd7239e90c3975b
JuanCRdrums <juankdrums14@gmail.com>
2022-02-24 16:23:01 -0500

    first commit

---
+++ API/Models/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Request
+    {
+        //recibo strings porque voy a trabajar con BigInteger
+        public string n1 { get; set; }
+        public string n2 { get; set; }
+    }
+}
---
+++ API/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Result
+    {
+        //reetorno un string porque pueden ser números grandes que no quepan en UInt64
+        public string result { get; set; }
+        public bool fibonacci { get; set; } = false;
+    }
+}
